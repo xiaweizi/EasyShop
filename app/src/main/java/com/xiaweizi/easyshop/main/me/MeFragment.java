1e7b6f09c0f67b950cf2668d38ec3ba5b736e25d
ljkj <1012126908@qq.com>
2017-02-07 20:53:37 +0800

    在我的fragment添加点击事件，直达登陆界面

--- app/src/main/java/com/xiaweizi/easyshop/main/me/MeFragment.java
+++ app/src/main/java/com/xiaweizi/easyshop/main/me/MeFragment.java
@@ -8,6 +8,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.xiaweizi.easyshop.R;
+import com.xiaweizi.easyshop.commons.ActivityUtils;
+import com.xiaweizi.easyshop.user.LoginActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by ljkj on 2017/2/7.
@@ -15,10 +20,21 @@ import com.xiaweizi.easyshop.R;
 
 public class MeFragment extends Fragment {
 
+    private ActivityUtils mActicityUtils;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_me, null);
+        ButterKnife.bind(this, view);
+        mActicityUtils = new ActivityUtils(this);
         return view;
     }
+
+    @OnClick({R.id.tv_person_info, R.id.tv_login, R.id.tv_person_goods, R.id.tv_goods_upload})
+    public void onClick(View view) {
+        mActicityUtils.startActivity(LoginActivity.class);
+        // TODO: 2017/2/7 0007 需要判断是否登录，从而决定跳转位置
+    }
+
 }
